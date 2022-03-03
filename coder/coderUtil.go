d1e2b21739e18282402d4e3d54dc748f30b1562a
wuhen <15210204903@163.com>
2022-03-03 15:29:16 +0800

    增加签名验签以及公私钥转换的方法

--- coder/coderUtil.go
+++ coder/coderUtil.go
@@ -29,13 +29,9 @@ func Sm4RandomKey() []byte {
 
 // Sm2Encrypt Sm2加密
 func Sm2Encrypt(pemPublicKey string, data []byte) ([]byte, error) {
-	block, _ := pem.Decode([]byte(pemPublicKey))
-	if block == nil {
-		return nil, errors.New("解析公钥信息失败")
-	}
-	key, err := x509.ParseSm2PublicKey(block.Bytes)
+	key, err := ConvertPemToPublicKey(pemPublicKey)
 	if err != nil {
-		return nil, errors.New("转换公钥信息失败")
+		return nil, err
 	}
 	encrypt, err := sm2.Encrypt(key, data, cryptoRand.Reader)
 	if err != nil {
@@ -46,7 +42,57 @@ func Sm2Encrypt(pemPublicKey string, data []byte) ([]byte, error) {
 
 // Sm2Decrypt sn2解密
 func Sm2Decrypt(pemPrivateKey string, data []byte) ([]byte, error) {
-	block, _ := pem.Decode([]byte(pemPrivateKey))
+	key, err := ConvertPemToPrivateKey(pemPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	decrypt, err := sm2.Decrypt(key, data)
+	if err != nil {
+		return nil, errors.New("解密数据失败")
+	}
+	return decrypt, nil
+}
+
+func Sm2Sign(pemPrivateKey string, data []byte) ([]byte, error) {
+	key, err := ConvertPemToPrivateKey(pemPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return Sm2SignWithKey(key, data)
+}
+
+func Sm2VerifySign(pemPubKey string, sign, data []byte) (bool, error) {
+	key, err := ConvertPemToPublicKey(pemPubKey)
+	if err != nil {
+		return false, err
+	}
+	return Sm2VerifySignWithKey(key, sign, data), nil
+}
+
+func Sm2VerifySignWithKey(publicKey *sm2.PublicKey, sign, data []byte) bool {
+	return publicKey.Verify(sign, data)
+}
+
+func Sm2SignWithKey(privateKey *sm2.PrivateKey, data []byte) ([]byte, error) {
+	return privateKey.Sign(cryptoRand.Reader, data, nil)
+}
+
+func ConvertPemToPublicKey(pubPem string) (*sm2.PublicKey, error) {
+	block, _ := pem.Decode([]byte(pubPem))
+	if block == nil {
+		return nil, errors.New("解析公钥信息失败")
+	}
+	key, err := x509.ParseSm2PublicKey(block.Bytes)
+	if err != nil {
+		return nil, errors.New("转换公钥信息失败")
+	}
+
+	return key, nil
+}
+
+func ConvertPemToPrivateKey(privateKeyPem string) (*sm2.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKeyPem))
 	if block == nil {
 		return nil, errors.New("解析私钥信息失败")
 	}
@@ -55,11 +101,22 @@ func Sm2Decrypt(pemPrivateKey string, data []byte) ([]byte, error) {
 	if err != nil {
 		return nil, errors.New("转换私钥信息失败")
 	}
-	decrypt, err := sm2.Decrypt(key, data)
+
+	return key, nil
+}
+
+func ConvertPemToPubAndPriKey(pubKeyPem, privateKeyPem string) (*sm2.PublicKey, *sm2.PrivateKey, error) {
+	pubKey, err := ConvertPemToPublicKey(pubKeyPem)
 	if err != nil {
-		return nil, errors.New("解密数据失败")
+		return nil, nil, err
 	}
-	return decrypt, nil
+
+	privateKey, err := ConvertPemToPrivateKey(privateKeyPem)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return pubKey, privateKey, nil
 }
 
 // Sm2DecryptByBase64Data sm2解密，数据格式为Base64
